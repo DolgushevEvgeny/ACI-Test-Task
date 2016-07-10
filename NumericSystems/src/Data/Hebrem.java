662c85aaf317416805a21bc0d582de14ae0b4834
Dolgushev Evgeny <baraxlush1995@gmail.com>
2016-07-10 22:29:40 +0300

    Refactored.
    Split word to chars.

--- NumericSystems/src/Data/Hebrem.java
+++ NumericSystems/src/Data/Hebrem.java
@@ -1,25 +1,45 @@
 package Data;
 
-//import Factory.VocabularyToken;
 import java.util.ArrayList;
 
 public class Hebrem {
     private static ArrayList<String> tokens = null;
     private static int index = 0;
+    
     private static final char LETTER_H = 'h';
     private static final char LETTER_APOSTROPHE = '`';
+    private static final char LETTER_QUOTE = '\'';
     private static final char LETTER_T = 't';
     private static final char LETTER_K = 'k';
     private static final char LETTER_S = 's';
     private static final char LETTER_L = 'l';
+    private static final char LETTER_B = 'b';
+    private static final char LETTER_G = 'g';
+    private static final char LETTER_D = 'd';
+    private static final char LETTER_W = 'w';
+    private static final char LETTER_Z = 'z';
+    private static final char LETTER_X = 'x';
     
     
     static public double convert(String input) {
+        input = input.toLowerCase();
+        try {
+            split(input);
+        } catch (NumberFormatException ex) {
+            throw new NumberFormatException(ex.toString());
+        }
+        try {
+            parse();
+        } catch (NumberFormatException ex) {
+            throw new NumberFormatException(ex.toString());
+        }
+        
+        tokens.clear();
         return 0;
     }
     
-    static private boolean parse(String input) {
-        for (int i = 0; i < input.length(); ++i) {
+    static private boolean parse() {
+        for (int i = tokens.size() - 1; i > 0; --i) {
             
         }
         return true;
@@ -32,10 +52,11 @@ public class Hebrem {
         for (index = 0; index < inputArray.length; ++index) {
             try {
                 switch (inputArray[index]){
-                    case LETTER_T: checkLetterT(inputArray); ++index; break;
+                    case LETTER_T: checkLetterT(inputArray); break;
                     case LETTER_K: checkLetterKSL(inputArray, LETTER_H); break;
                     case LETTER_S: checkLetterKSL(inputArray, LETTER_APOSTROPHE); break;
                     case LETTER_L: checkLetterKSL(inputArray, LETTER_H); break;
+                    case LETTER_QUOTE: checkLetterQuote(inputArray); break;
                     default:
                         tokens.add(String.valueOf(inputArray[index]));
                         break;
@@ -43,7 +64,6 @@ public class Hebrem {
             } catch (NumberFormatException ex) {
                 throw new NumberFormatException(ex.toString());
             }
-            
         }
         
         index = 0;
@@ -51,7 +71,7 @@ public class Hebrem {
     
     static private void checkLetterT(char[] input) {
         if ((input[index + 1] == LETTER_APOSTROPHE) || (input[index + 1] == LETTER_H)) {
-            tokens.add(String.valueOf(input[index] + input[index + 1]));
+            addToken(input[index], input[index + 1]);
             return;
         }
         
@@ -60,11 +80,65 @@ public class Hebrem {
     
     static private void checkLetterKSL(char[] input, char letter) {
         if (input[index + 1] == letter) {
-            tokens.add(String.valueOf(input[index] + input[index + 1]));
-            ++index;
+            addToken(input[index], letter);
         } else {
-            tokens.add(String.valueOf(input[index]));
+            addToken(input[index]);
         }
+    }
+    
+    static private void checkLetterQuote(char[] input) {
+        if (input[index + 1] == LETTER_QUOTE) {
+            addToken(input[index], LETTER_QUOTE);
+            return;
+        }
+        if (input[index + 1] == LETTER_B) {
+            addToken(input[index], LETTER_B);
+            return;
+        }
+        if (input[index + 1] == LETTER_G) {
+            addToken(input[index], LETTER_G);
+            return;
+        }
+        if (input[index + 1] == LETTER_D) {
+            addToken(input[index], LETTER_D);
+            return;
+        }
+        if (input[index + 1] == LETTER_H) {
+            addToken(input[index], LETTER_H);
+            return;
+        }
+        if (input[index + 1] == LETTER_W) {
+            addToken(input[index], LETTER_W);
+            return;
+        }
+        if (input[index + 1] == LETTER_Z) {
+            addToken(input[index], LETTER_Z);
+            return;
+        }
+        if (input[index + 1] == LETTER_X) {
+            addToken(input[index], LETTER_X);
+            return;
+        }
+        if ((input[index + 1] == LETTER_T) && (input[index + 2] == LETTER_QUOTE)) {
+            addToken(input[index], LETTER_T, LETTER_QUOTE);
+            return;
+        }
+        
+        addToken(input[index]);
+    }
+    
+    static private void addToken(char letter) {
+        tokens.add(String.valueOf(letter));
+    }
+    
+    static private void addToken(char letter1, char letter2) {
+        tokens.add(String.valueOf(letter1) + String.valueOf(letter2));
+        ++index;
+    }
+    
+    static private void addToken(char letter1, char letter2, char letter3) {
+        tokens.add(String.valueOf(letter1) + String.valueOf(letter2) + String.valueOf(letter3));
+        ++index;
     }
     
     /*static private void checkLetterK(char[] input) {
